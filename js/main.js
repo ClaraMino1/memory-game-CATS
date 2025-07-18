f3886e771668c7c248f388c960b125d930047817
Clara Miño <claraa.mino12@gmail.com>
2025-07-18 07:38:16 -0300

    cree las cards con sus imagenes de gatos del otro lado

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
-const container = document.getElementById("container")
-const url = "https://cataas.com/cat/"; //para luego concatenarle el id
+const container = document.getElementById("container");
+const url = "https://cataas.com/cat/"; //url para luego concatenar el id y generar la img
 let cards = [];
 
-async function getCatImages(numberOfImages) { 
+async function getCatImages(numberOfImages) {
   try {
     const response = await fetch(`https://cataas.com/api/cats?limit=${numberOfImages}`);
     const data = await response.json();
 
-    data.forEach(item => {
-      let imageId = item.id; //obtiene el id de la img
-      let imageUrl = url + imageId; //concatena la url con el id para poder acceder a la img
-      cards.push(imageUrl); //mete la img al array
+    data.forEach(item => { //por cada img que le llega filtra las que son png o jpeg
+      if (item.mimetype === 'image/jpeg' || item.mimetype === 'image/png') {
+        console.log("aceptada");
 
-        const img = document.createElement("img") //crea la img por cada elemento del array
-        img.classList.add("card-img")
-        img.src = imageUrl
-        img.alt = "imagen de gato"
+        let imageId = item.id; //obtiene el id de la img
+        let imageUrl = url + imageId; //concatena para generar una url
+        cards.push(imageUrl); //agrega al array
 
-        container.appendChild(img) //img en el dom
-    })      
+        const card = document.createElement("div");//crea card
+        card.classList.add("card");
+
+        const cardContent = document.createElement("div"); //crea cardContent
+        cardContent.classList.add("card__content");
+
+        const img = document.createElement("img"); //crea la img
+        img.classList.add("card-img");
+        img.src = imageUrl; //le asigna la img que hay en el array
+        img.alt = "imagen de gato";
+
+      
+        cardContent.appendChild(img);
+        card.appendChild(cardContent);
+        container.appendChild(card);
+      } else {
+        console.log("gif no aceptado");
+      }
+    });
 
   } catch (error) {
     console.error('Error al obtener las imágenes:', error);
   }
 }
 
-getCatImages(6);
+getCatImages(17);
